f68d4dfe8064f8c6a034e921f73d3f7f934302bf
NineOneFive <nuevounocinco@gmail.com>
2020-09-02 22:56:46 -0400

    Made commandhandling more robust

--- src/main/java/me/ryguy/discordapi/command/CommandHandler.java
+++ src/main/java/me/ryguy/discordapi/command/CommandHandler.java
@@ -5,30 +5,35 @@ import discord4j.core.object.entity.Message;
 import me.ryguy.discordapi.DiscordBot;
 import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CommandHandler {
     public CommandHandler() {
         DiscordBot.getBot().getGateway().on(MessageCreateEvent.class).subscribe(e -> {
             Message message = e.getMessage();
             if (e.getMessage().getEmbeds().size() != 0) return;
             if (!e.getMessage().getContent().startsWith(DiscordBot.getBot().getPrefix())) return;
-            Command cmd; //really lazy af way of making sure a command is valid and avoiding errors
+
+            if (!message.getAuthor().isPresent() || message.getAuthor().get().isBot())
+                return;
+
             try {
-                cmd = CommandManager.getCommand(message.getContent()
-                        .split(" ")[0]
-                        .substring(DiscordBot.getBot().getPrefix().length())
-                        .split("\n")[0]);
+                List<String> command = parseCommand(message.getContent());
+                Command cmd = CommandManager.getCommand(command.get(0));
+
+                if (cmd == null || !cmd.canExecute(message))
+                    return;
+
+                cmd.execute(message, command.get(0), command.subList(1, command.size()).toArray(new String[0]));
             } catch (Exception ex) {
-                return;
+                ex.printStackTrace();
             }
-            if (message.getAuthor().get().isBot())
-                return;
-            if (cmd == null)
-                return;
-            if (!cmd.canExecute(message)) {
-                return;
-            }
-            CommandManager.getCommand(message.getContent().split(" ")[0].split("\n")[0].substring(DiscordBot.getBot().getPrefix().length()))
-                    .execute(message, message.getContent().split(" ")[0].substring(DiscordBot.getBot().getPrefix().length()), ArrayUtils.remove(message.getContent().split(" "), 0));
         });
+    }
+
+    public static List<String> parseCommand(String input) {
+        return Arrays.stream(input.split("\\W+")).filter(it -> !it.equals("")).collect(Collectors.toList());
     }
 }
